7f65d85a6a47deac4aa90bb50763ba3faea07a51
tammyw25 <tammy.wong25@ycdsbk12.ca>
2024-02-08 12:31:16 -0500

    Updated PrintingComments.java

--- PrintingComments.java
+++ PrintingComments.java
@@ -8,14 +8,34 @@ class PrintingComments extends ConsoleProgram {
   public void run() {
 
     // Say hello
-    
-
+    System.out.println("Hi");
+    System.out.println("There!");
     // Say hello on the same line
-    
+    System.out.print("Hi");
+    System.out.print("There!");
     
     // multiline comment
-    
-    
+    /* 
+  * This is
+  * a multiline
+  * comment 
+  */
+  
+  // Draw a box
+ System.out.println("*************");
+ System.out.println("*           *");
+ System.out.println("*           *");
+ System.out.println("*           *");
+ System.out.println("*************");
+      
+ // Draw another box
+ /*
+ System.out.println("*************");
+ System.out.println("*           *");
+ System.out.println("*           *");
+ System.out.println("*           *");
+ System.out.println("*************");
+ */
 
   }
 }
